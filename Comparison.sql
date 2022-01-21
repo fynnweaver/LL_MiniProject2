bcfdf14ce6a16be4afd98d20e42ae281dab2c1cd
Fynn Davis <fynn.weaver@gmail.com>
2022-01-20 21:53:41 -0800

    Save csv files of conclusion into data folder

--- Comparison.sql
+++ Comparison.sql
@@ -10,25 +10,56 @@ categories: Entertainment, Dining, Health, Landmarks
 /*total venues*/
 SELECT SUM(total) FROM foursquare_data; 
 SELECT SUM(total) FROM yelp_data; 
-/* foursquare: 168, yelp: 180*/
+/* foursquare: 394, yelp: 313*/
 
 /*Category of venues*/
-SELECT Category, COUNT(*) FROM foursquare_data
-    GROUP BY Category;
-SELECT category, COUNT(*) FROM yelp_data
-    GROUP BY category;
+SELECT Category, COUNT(*) total FROM foursquare_data
+    GROUP BY Category
+    ORDER BY total DESC;
+SELECT category, COUNT(*) total FROM yelp_data
+    GROUP BY category
+    ORDER BY total DESC;
+
+SELECT four.category, four.total f_total, yelp.total y_total 
+FROM(
+    SELECT Category AS category, COUNT(*) total 
+        FROM foursquare_data
+        GROUP BY Category
+        ORDER BY total DESC) four 
+    LEFT OUTER JOIN(
+        SELECT category, COUNT(*) total 
+        FROM yelp_data
+        GROUP BY category
+        ORDER BY total DESC) yelp 
+        ON four.category = yelp.category;
 
 /*Comparing the dining category*/
 SELECT title, total FROM foursquare_data
-    WHERE Category == 'Dining';
+    WHERE Category == 'Business and Professional Services';
 SELECT title, total FROM yelp_data
-    WHERE category == 'Dining';
+    WHERE category == 'Business and Professional Services';
 
 /*types of venues*/
 SELECT title, COUNT(*) total FROM foursquare_data
     GROUP BY title
-    ORDER BY total DESC;
+    ORDER BY total DESC
+    LIMIT 10;
 SELECT title, COUNT(*) total FROM yelp_data
     GROUP BY title
-    ORDER BY total DESC;
-/*foursquare: 74, yelp: 82*/
+    ORDER BY total DESC
+    LIMIT 10;
+/*foursquare: 200, yelp: 149*/
+
+/*In terms of using API as a source of broadscale
+venue data (what's in the area), how do these two compare?
+
+Foursquare is better to get an idea of the types of venues,
+it also provides more Landmark and event data for the area.
+
+Another major limitation is that the results are randomized and returned
+by a limit of 50. With a 1km radius that does not seem to occur however.
+
+Yelp is better to get a specific and distinct venue data.
+Results do not overlap between categories
+    (ex: a building under Dining > restauraunt and under
+    Entertainment > theatre will count once for each)
